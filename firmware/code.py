24c185dee52f07fe1a8ed9437e65696578a399d9
euvalennn <97848208+euvalennn@users.noreply.github.com>
2025-08-24 23:53:22 -0300

    updated firmware, only thing changed is neopixel color

--- firmware/code.py
+++ firmware/code.py
@@ -5,7 +5,7 @@ import neopixel
 pixels = neopixel.NeoPixel(board.GP25, 1)
 
 while True:
-    pixels.fill((255, 0, 0))
+    pixels.fill((85, 57, 204))
     time.sleep(0.5)
     pixels.fill((0, 0, 0))
     time.sleep(0.5)
